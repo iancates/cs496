f795e6adfc0e088022978058972cdeaffe0f81dd
Ian Thomas Cates <catesia@oregonstate.edu>
2016-06-26 23:51:22 +0000

    test

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app.config['DEBUG'] = True
 def hello():
   time_url = "http://www.timeapi.org/pdt/now?\a \b \d \I:\M:\S \Z \Y"
   current_time = urlfetch.fetch(time_url).content
-  return "test {0}".format(current_time)
+  return current_time
   
 
 @app.errorhandler(404)
