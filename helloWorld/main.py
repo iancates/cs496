b45b842a9efaa2469904d5b2465a03f7038da0d2
Ian Thomas Cates <catesia@oregonstate.edu>
2016-06-26 22:47:31 +0000

    testing

--- helloWorld/main.py
+++ helloWorld/main.py
@@ -18,7 +18,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hell, !')
+        self.response.write('The cat is super cool, !')
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
