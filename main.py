bca4b268ed9c312c760c7b79a3cd72a40c35f531
Ian Thomas Cates <catesia@oregonstate.edu>
2016-06-26 23:45:59 +0000

    rte

--- main.py
+++ main.py
@@ -9,14 +9,7 @@ app.config['DEBUG'] = True
 
 @app.route('/')
 def hello():
-   """
-    Return the current time and weather by making two separate requests to:
-    (1)timeapi.org
-    (2)openweathermap.org
-    """
-    # first request = time
-    time_url = "http://www.timeapi.org/pdt/now?\a \b \d \I:\M:\S \Z \Y"
-    current_time = urlfetch.fetch(time_url).content
+
     return "Test"
 
 @app.errorhandler(404)
