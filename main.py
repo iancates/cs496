9e4f2d0dccadc82e389c364ac05f2a14b24c61ee
Ian Thomas Cates <catesia@oregonstate.edu>
2016-06-26 23:29:47 +0000

    Added Weather

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from flask import Flask
-import datetime
+import json
+from google.appengine.api import urlfetch
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-# Note: We don't need to call run() since our application is embedded within
-# the App Engine WSGI application server.
 
 
-current_time = datetime.datetime.now().time()
-current_time.isoformat()
+
 @app.route('/')
 def hello():
-    """Return a friendly HTTP greeting."""
-    return current_time
-
+   url = "http://api.openweathermap.org/data/2.5/weather?zip=83843,us&appid=a0b1ce364984d3df0147915a50d2ce7d"
+    weatherFetch = urlfetch.fetch(url).content
+    weather = json.loads(weatherFetch)
+    temp = weather["main"]["temp"]
+    
+    return "The current temperature is {0}".format(temp) 
 
 @app.errorhandler(404)
 def page_not_found(e):
