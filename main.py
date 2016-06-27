5a75a589fac64b1980264eeed6f08da5642b8d6e
Ian Thomas Cates <catesia@oregonstate.edu>
2016-06-27 00:51:56 +0000

    time

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask
-import json
 from google.appengine.api import urlfetch
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -10,7 +9,6 @@ app.config['DEBUG'] = True
 @app.route('/')
 def hello():
     url = 'http://www.timeapi.org/pdt/now?format=%25a%20%25b%20%25d%20%25I:%25M:%25S%20%25Z%20%25Y'
-    #url = 'http://www.google.com/humans.txt'
     result = urlfetch.fetch(url)
     return result.content
 
@@ -20,5 +18,4 @@ def hello():
 def page_not_found(e):
     """Return a custom 404 error."""
     return 'Sorry, nothing at this URL.', 404
-
 
