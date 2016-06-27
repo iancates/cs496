d3b2a09356afc69bb2a78897e54c8b9e0fde2e76
iancates <Ian@ians-MacBook-Pro-3.local>
2016-06-26 17:47:59 -0700

    time

--- main.py
+++ main.py
@@ -9,14 +9,16 @@ app.config['DEBUG'] = True
 
 @app.route('/')
 def hello():
-  time_url = "http://www.timeapi.org/pdt/now?\a \b \d \I:\M:\S \Z \Y"
-  current_time = urlfetch.fetch(time_url).content
-  print "test"
-  return "Greetings from Moscow, ID. It is currently {0}, and the temperature is approximately Kelvin." \
-           .format(current_time)
-  
+    url = 'http://www.timeapi.org/pdt/now?format=%25a%20%25b%20%25d%20%25I:%25M:%25S%20%25Z%20%25Y'
+    #url = 'http://www.google.com/humans.txt'
+    result = urlfetch.fetch(url)
+    return result.content
+
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
     return 'Sorry, nothing at this URL.', 404
+
+
